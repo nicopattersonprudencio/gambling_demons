9fcc4bd946c252d3246d692da8ddf0adecacff32
nicolas patterson <nicopattersonprudencio@gmail.com>
2026-08-30 14:19:53 -0300

    escena simbolos creada y mapa modificado

---
+++ scripts/mapa.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+#posiciones -> (0, -398), (-283,283), (283,283)
---
+++ scripts/simbolos.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+"""
+SIMBOLOS
+
+res://sprites/cofre_simbolo.png -> scale = 0.3
+res://sprites/demonio_simbolo.png -> scale = 0.5
+res://sprites/bolsa_dinero.png -> scale = 0.15
+
+"""
